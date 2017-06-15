abb3c3d56ec20f6b0168a150362ce291bd9e3a76
Xavier Xie <xieyuehao@gmail.com>
2017-06-15 23:31:13 +1000

    Added first test

---
+++ test/spec/masterControllerSpec.ts
@@ -0,0 +1,8 @@
+/// <reference path="../../typings/index.d.ts" />
+
+describe("my first test", () => {
+
+    it('my first exception', () => {
+        expect(true).toBeTruthy('What? Why was true not truth?');
+    });  
+});
